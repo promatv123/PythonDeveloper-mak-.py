2fdcf772dffce3a75ed03ffa8efe6645ac628937
promatv123 <matvejorlov840@gmail.com>
2025-11-29 18:53:23 +0300

    maksim

---
+++ Lesson_10/main.py
@@ -0,0 +1,34 @@
+from tkinter import*
+
+mainWindow = Tk()
+width = 700
+height = 400
+
+
+screen_width = mainWindow.winfo_screenwidth()
+screen_height = mainWindow.winfo_screenheight()
+
+x_offset = (screen_width - width) // 2
+y_offset = (screen_height - height) // 2
+
+mainWindow.geometry(f"{width}x{height}+{x_offset}+{y_offset}")
+
+mainWindow.title("Викторина")
+mainWindow.resizable(False, False)
+
+QUEST = Label(text="", font=("Arial", 16))
+QUEST.place(anchor="center",relx=0.5, rely=0.15)
+
+info = Label(text="", font=("Arial", 12), fg="red")
+info.place(anchor="center",relx=0.5, rely=0.25)
+
+buttons = []
+
+for i in range(1, 5):
+    btn = Button(width=50, height=1, font=("Arial", 14))
+    btn.place(anchor="center",relx=0.5, rely=0.25+0.15*i)
+    buttons.append(btn)
+
+
+
+mainWindow.mainloop()
